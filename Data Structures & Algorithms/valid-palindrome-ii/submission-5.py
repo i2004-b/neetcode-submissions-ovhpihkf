00d85d1a207a2cfe496afc7d9dc31ffc83391b1e
Ivan <ivanbyju.2023@gmail.com>
2026-06-08 12:00:32 -0400

    Add: valid-palindrome-ii - submission-5

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-5.py
@@ -0,0 +1,27 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        
+        # Helper function to figure out if a palindrome
+        def is_palin(s, l, r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                
+                l, r = l + 1, r - 1
+
+            return True
+
+
+        # Declare pointers
+        l, r = 0, len(s) - 1
+
+        # Iterate while l does not pass r
+        while l < r:
+            # If the letters are not equal, run the function and return if either way is a palindrome
+            if s[l] != s[r]:
+                return is_palin(s, l, r - 1) or is_palin(s, l + 1, r)
+
+            l += 1
+            r -= 1
+
+        return True
