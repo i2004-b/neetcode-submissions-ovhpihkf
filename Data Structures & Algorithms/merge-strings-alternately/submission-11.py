6943f9e403768bc8949f83931d82ef189b379ffc
Ivan <ivanbyju.2023@gmail.com>
2026-06-11 13:36:50 -0400

    Add: merge-strings-alternately - submission-11

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-11.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        # Declare array to hold letters the join
+        arr = []
+
+        # Declare pointers pointing to each word first letter
+        i, j = 0, 0
+
+        # Iterate while both pointers in range of the words
+        while i < len(word1) and j < len(word2):
+            arr.append(word1[i])
+            arr.append(word2[j])
+
+            i, j = i + 1, j + 1
+
+        # Add remaining characters
+        if i < len(word1):
+            arr.append(word1[i:])
+        elif j < len(word2):
+            arr.append(word2[j:])
+
+        return "".join(arr)
