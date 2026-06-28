fb2d440261c8395329d434426e029809f64ea7b4
Ivan <ivanbyju.2023@gmail.com>
2026-06-28 18:27:29 -0400

    Add: maximum-frequency-stack - submission-2

---
+++ Data Structures & Algorithms/maximum-frequency-stack/submission-2.py
@@ -0,0 +1,32 @@
+class FreqStack:
+    """
+    Most Optimal Solution: Use a dictionary with stacks as values to the keys
+    Push --> O(1)
+    Pop --> O(1)
+    """
+    def __init__(self):
+        self.cnt = {}
+        self.stacks = {}
+        self.max_cnt = 0
+
+    def push(self, val: int) -> None:
+        self.cnt[val] = 1 + self.cnt.get(val, 0)
+        # Add stack if needed and update max_cnt
+        if self.cnt[val] > self.max_cnt:
+            self.max_cnt = self.cnt[val]
+            self.stacks[self.max_cnt] = []
+        self.stacks[self.cnt[val]].append(val)
+
+
+    def pop(self) -> int:
+        res = self.stacks[self.max_cnt].pop()
+        self.cnt[res] -= 1
+        if not self.stacks[self.max_cnt]:
+            self.max_cnt = self.cnt[res]
+
+        return res
+
+# Your FreqStack object will be instantiated and called as such:
+# obj = FreqStack()
+# obj.push(val)
+# param_2 = obj.pop()
