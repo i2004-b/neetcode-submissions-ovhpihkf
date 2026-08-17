c156e85ad3a2d3998b5c640b72b64d22540b035a
Ivan <ivanbyju.2023@gmail.com>
2026-08-17 08:32:39 -0400

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,48 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # Create map for the course numbers to lists that will hold the pre-reqs
+        pre_map = {i: [] for i in range(numCourses)}
+
+        # Add the pre_reqs to the lists
+        for crs, pre in prerequisites:
+            pre_map[crs].append(pre)
+
+        # Have a visit set for courses that have been visited
+        visit = set()
+
+        # fulfilled
+        fulfilled = set()
+
+        # Write dfs algorithm that goes through the courses
+        def dfs(crs):
+            # If the course is in the visit set, return False
+            if crs in visit:
+                return False
+            # If the course's list is empty, return True (everything satsified)
+            if crs in fulfilled:
+                return True
+
+            # Add the course to the visit set
+            visit.add(crs)
+
+            # Run dfs for each pre-req
+            for pre in pre_map[crs]:
+                # If false, return False
+                if not dfs(pre):
+                    return False
+
+            # Remove the course from visit
+            visit.remove(crs)
+            # Add to fulfilled
+            fulfilled.add(crs)
+            
+
+            # Return true
+            return True
+
+        # Call dfs on the courses
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+
+        return True
