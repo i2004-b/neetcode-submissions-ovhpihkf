2aec1dbae88165718a54bfd7142b1fc2517901e1
Ivan <ivanbyju.2023@gmail.com>
2026-09-03 15:20:10 -0400

    Add: count-paths - submission-2

---
+++ Data Structures & Algorithms/count-paths/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # Recursive solution with memoization
+
+        # Declare 2-d grid to store paths from that location
+        grid = [[-1 for _ in range(n)] for _ in range(m)]
+
+        def dfs(x, y):
+            if x == m or y == n:
+                return 0
+            if x == m - 1 and y == n - 1:
+                return 1
+            if grid[x][y] > -1:
+                return grid[x][y]
+
+            res = 0
+            res += dfs(x + 1, y)
+            res += dfs(x, y + 1)
+
+            grid[x][y] = res
+
+            return res
+
+        return dfs(0, 0)
