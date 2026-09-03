204668c8718bcf8f44b0940b38f63a8957665989
Ivan <ivanbyju.2023@gmail.com>
2026-09-03 15:41:00 -0400

    Add: count-paths - submission-6

---
+++ Data Structures & Algorithms/count-paths/submission-6.py
@@ -0,0 +1,14 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # Create 2-D grid
+        grid = [1] * n
+        prev = [1] * n
+
+        for i in range(m - 1):
+            # Iterate backwards in grid
+            for j in range(n - 2, -1, -1):
+                prev[j] = grid[j] + prev[j + 1]
+            # Set grid to prev
+            grid = prev
+
+        return grid[0]
